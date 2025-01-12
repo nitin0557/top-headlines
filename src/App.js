63297d3b7d857959c63a927724b83d77098b0d07
nit789 <nitinkrsingh789@gmail.com>
2025-01-13 02:14:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -32,14 +32,14 @@ export default class App extends Component {
         progress={this.state.progress} 
       />
         <Switch>
-          <Route exact path="/"><News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="in" category="general"/></Route> 
-          <Route exact path="/business"><News setProgress={this.setProgress} key="business" pageSize={this.pageSize} country="in" category="business"/></Route> 
-          <Route exact path="/entertainment"><News setProgress={this.setProgress} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/></Route> 
-          <Route exact path="/general"><News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="in" category="general"/></Route> 
-          <Route exact path="/health"><News setProgress={this.setProgress} key="health" pageSize={this.pageSize} country="in" category="health"/></Route> 
-          <Route exact path="/science"><News setProgress={this.setProgress} key="science" pageSize={this.pageSize} country="in" category="science"/></Route> 
-          <Route exact path="/sports"><News setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country="in" category="sports"/></Route> 
-          <Route exact path="/technology"><News setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country="in" category="technology"/></Route> 
+          <Route exact path="/top-headlines"><News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="in" category="general"/></Route> 
+          <Route exact path="/top-headlines"><News setProgress={this.setProgress} key="business" pageSize={this.pageSize} country="in" category="business"/></Route> 
+          <Route exact path="/top-headlines"><News setProgress={this.setProgress} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/></Route> 
+          <Route exact path="/top-headlines"><News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="in" category="general"/></Route> 
+          <Route exact path="/top-headlines"><News setProgress={this.setProgress} key="health" pageSize={this.pageSize} country="in" category="health"/></Route> 
+          <Route exact path="/top-headlines"><News setProgress={this.setProgress} key="science" pageSize={this.pageSize} country="in" category="science"/></Route> 
+          <Route exact path="/top-headlines"><News setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country="in" category="sports"/></Route> 
+          <Route exact path="/top-headlines"><News setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country="in" category="technology"/></Route> 
         </Switch>
         </Router>
       </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,22 +8,22 @@ export class NavBar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <Link className="navbar-brand" to="/">NewsMonkey</Link>
+                        <Link className="navbar-brand" to="/top-headlines">NewsMonkey</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                                <Link className="nav-link" aria-current="page" to="/top-headlines">Home</Link>
                             </li>
-                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/top-headlines">Business</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/top-headlines">Entertainment</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/top-headlines">General</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/top-headlines">Health</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/top-headlines">Science</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/top-headlines">Sports</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/top-headlines">Technology</Link></li>
                              
                           
                         </ul>
